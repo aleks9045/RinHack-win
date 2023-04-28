57ba128b61cd88ee918a25cb3417f25b8bd7c388
aleks <aleksey9045@gmail.com>
2023-04-28 10:39:24 +0300

    alggg

--- app1/algorithms.py
+++ app1/algorithms.py
@@ -11,7 +11,12 @@ def main_algorithm(new, old):
     hsh = hashlib.sha3_512(stroka.encode('utf-8')).hexdigest()
     for i in old:
         if i['fp'] == hsh:
-            user_data = list(UserUser.objects.all().filter(fp=hsh).values('username', 'email'))
-            files_data = list(Files.objects.all().filter(email=user_data[0]['email']).values().all())
+            print(list(UserUser.objects.all().values()))
+            if not list(UserUser.objects.all().values()):
+                user_data = ''
+                files_data = ''
+            else:
+                user_data = list(UserUser.objects.all().filter(fp=hsh).values('username', 'email'))
+                files_data = list(Files.objects.all().filter(email=user_data[0]['email']).values().all())
             return [True, user_data, files_data]
     return [False, hsh]
